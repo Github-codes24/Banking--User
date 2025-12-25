4c316d63d7de8028a258e0a51f0ecc717ee74965
sahil vasoya <ssvasoya901@gmail.com>
2025-12-25 15:31:01 +0530

    fix issue

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -43,20 +43,20 @@ export default function RootLayout() {
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-        <Stack.Screen name="splash" options={{ headerShown : false}} />
+        <Stack.Screen name="splsh" options={{ headerShown : false}} />
         <Stack.Screen name="getstarted" options={{ headerShown : false}} />
         <Stack.Screen name="forgot" options={{ headerShown : false}} />
-        <Stack.Screen name="login" options={{ headerShown : false}} />
+        <Stack.Screen name="logn" options={{ headerShown : false}} />
         <Stack.Screen name="verify" options={{  header: () => (<SafeAreaView edges={['top']} style={{backgroundColor:"#fff"}}><CommonHeader title='OTP Verification'/></SafeAreaView>)}} />
         <Stack.Screen name="reset" options={{  header: () => (<SafeAreaView edges={['top']} style={{backgroundColor:"#fff"}}><CommonHeader title='Reset Password'/></SafeAreaView>)}} />
         <Stack.Screen name="loginpin" options={{  header: () => (<SafeAreaView edges={['top']} style={{backgroundColor:"#fff"}}><CommonHeader title='Login PIN'/></SafeAreaView>)}} />
         <Stack.Screen name="tip" options={{  header: () => (<SafeAreaView edges={['top']} style={{backgroundColor:"#fff"}}><CommonHeader title='Security Tips'/></SafeAreaView>)}} />
         <Stack.Screen name="home" options={{ headerShown : false}} />
-        <Stack.Screen name="account" options={{  header: () => (<SafeAreaView edges={['top']} style={{backgroundColor:"#fff"}}><CommonHeader title='Accounts'/></SafeAreaView>)}} />
-        <Stack.Screen name="acStatement" options={{  header: () => (<SafeAreaView edges={['top']} style={{backgroundColor:"#fff"}}><CommonHeader title='Accounts Statement'/></SafeAreaView>)}} />
-        <Stack.Screen name="fdeposite" options={{  header: () => (<SafeAreaView edges={['top']} style={{backgroundColor:"#fff"}}><CommonHeader title='Open Fixed Deposit'/></SafeAreaView>)}} />
+        <Stack.Screen name="accont" options={{  header: () => (<SafeAreaView edges={['top']} style={{backgroundColor:"#fff"}}><CommonHeader title='Accounts'/></SafeAreaView>)}} />
+        <Stack.Screen name="acStaement" options={{  header: () => (<SafeAreaView edges={['top']} style={{backgroundColor:"#fff"}}><CommonHeader title='Accounts Statement'/></SafeAreaView>)}} />
+        <Stack.Screen name="fdeosite" options={{  header: () => (<SafeAreaView edges={['top']} style={{backgroundColor:"#fff"}}><CommonHeader title='Open Fixed Deposit'/></SafeAreaView>)}} />
         <Stack.Screen name="redeposite" options={{  header: () => (<SafeAreaView edges={['top']} style={{backgroundColor:"#fff"}}><CommonHeader title='Open Recurring Deposit'/></SafeAreaView>)}} />
-        <Stack.Screen name="qrscreen" options={{  header: () => (<SafeAreaView edges={['top']} style={{backgroundColor:"#fff"}}><CommonHeader title='Qr Screen'/></SafeAreaView>)}} />
+        <Stack.Screen name="qrreen" options={{  header: () => (<SafeAreaView edges={['top']} style={{backgroundColor:"#fff"}}><CommonHeader title='Qr Screen'/></SafeAreaView>)}} />
         <Stack.Screen name="thistory" options={{  header: () => (<SafeAreaView edges={['top']} style={{backgroundColor:"#fff"}}><CommonHeader title='Transaction History'/></SafeAreaView>)}} />
       </Stack>
       <StatusBar style="auto" />
